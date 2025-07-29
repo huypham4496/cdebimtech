2f7f0f1b6a6dfc2780c9a3d2de5604ed5edbe4a8
huypham4496 <huypham@bimtech.edu.vn>
2025-07-29 11:15:11 +0700

    Update login.php

--- pages/login.php
+++ pages/login.php
@@ -32,7 +32,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Đăng nhập | CDE</title>
-  <link rel="stylesheet" href="../assets/css/auth.css">
+  <link rel="stylesheet" href="../assets/css/login.css">
 </head>
 <body class="login-page">
   <div class="login-wrapper">
