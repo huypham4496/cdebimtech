15c2c5744f66746bfa0ea0be61a10f61fa4dad56
huypham4496 <huypham@bimtech.edu.vn>
2025-07-29 11:07:06 +0700

    Create sidebar.php

---
+++ includes/sidebar.php
@@ -0,0 +1,32 @@
+<?php
+// includes/sidebar.php
+
+/** Render sidebar navigation */
+function renderSidebar() {
+    $user = \$_SESSION['user'];
+    return <<<HTML
+<aside class="sidebar">
+  <div class="brand">
+    <img src="assets/images/logo.png" alt="CDE Logo">
+    <span>Nova CDE</span>
+  </div>
+  <nav class="nav-menu">
+    <ul>
+      <li class="active"><a href="index.php">Overview</a></li>
+      <li><a href="#">Project</a></li>
+      <li><a href="#">Notification</a></li>
+      <li><a href="#">Member</a></li>
+      <li><a href="#">Meetings</a></li>
+      <li><a href="#">Work diary</a></li>
+      <li><a href="#">Activity History</a></li>
+      <li><a href="#">Settings</a></li>
+    </ul>
+  </nav>
+  <div class="user-info">
+    <img src="<?php echo \$user['avatar']; ?>" alt="Avatar">
+    <span><?php echo htmlspecialchars(\$user['name']); ?></span>
+    <a href="pages/login.php?logout=1">Sign out</a>
+  </div>
+</aside>
+HTML;
+}
