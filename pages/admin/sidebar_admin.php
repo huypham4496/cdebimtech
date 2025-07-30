f728a86550881d609261897f4c1a34b27b437230
huypham4496 <huypham@bimtech.edu.vn>
2025-07-30 08:36:51 +0700

    Create sidebar_admin.php

---
+++ pages/admin/sidebar_admin.php
@@ -0,0 +1,18 @@
+<?php
+// pages/admin/sidebar_admin.php
+// Session already started
+?>
+<div class="sidebar-admin">
+  <div class="sidebar-header">
+    <h2>AdminCP</h2>
+  </div>
+  <nav class="sidebar-nav-admin">
+    <ul>
+      <li class="active"><a href="index.php"><i class="fas fa-users-cog"></i> User Management</a></li>
+      <li><a href="subscriptions_info.php"><i class="fas fa-info-circle"></i> Subscriptions Info</a></li>
+      <li><a href="subscriptions.php"><i class="fas fa-file-contract"></i> Subscriptions</a></li>
+      <li><a href="payments.php"><i class="fas fa-credit-card"></i> Payments</a></li>
+      <li><a href="payment_requests.php"><i class="fas fa-hand-holding-usd"></i> Payment Requests</a></li>
+    </ul>
+  </nav>
+</div>
