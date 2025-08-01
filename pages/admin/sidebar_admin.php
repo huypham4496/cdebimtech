6c9c1350da683663bcdf152f12a704feb6c01e3b
huypham4496 <huypham@bimtech.edu.vn>
2025-08-01 16:28:14 +0700

    Update sidebar_admin.php

--- pages/admin/sidebar_admin.php
+++ pages/admin/sidebar_admin.php
@@ -31,6 +31,7 @@ $currentPage = basename($_SERVER['SCRIPT_NAME']);
       <li class="<?= $currentPage === 'index.php' ? 'active' : '' ?>"><a href="index.php"><i class="fas fa-users-cog"></i> User Management</a></li>
       <li class="<?= $currentPage === 'subscriptions_info.php' ? 'active' : '' ?>"><a href="subscriptions_info.php"><i class="fas fa-info-circle"></i> Subscriptions Info</a></li>
       <li class="<?= $currentPage === 'subscriptions.php' ? 'active' : '' ?>"><a href="subscriptions.php"><i class="fas fa-file-contract"></i> Subscriptions</a></li>
+      <li class="<?= $currentPage === 'subscription_features.php' ? 'active' : '' ?>"><a href="subscription_features.php"><i class="fas fa-sliders-h"></i> Subscription Features</a></li>
       <li class="<?= $currentPage === 'payments.php' ? 'active' : '' ?>"><a href="payments.php"><i class="fas fa-credit-card"></i> Payments</a></li>
       <li class="<?= $currentPage === 'payment_requests.php' ? 'active' : '' ?>"><a href="payment_requests.php"><i class="fas fa-hand-holding-usd"></i> Payment Requests</a></li>
     <li class="<?= $currentPage === 'voucher.php' ? 'active' : '' ?>"><a href="voucher.php"><i class="fas fa-ticket-alt"></i> Vouchers</a></li>
