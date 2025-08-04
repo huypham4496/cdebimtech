21b0262a0dc76fece3da27de69bac28374cb837a
huypham4496 <huypham@bimtech.edu.vn>
2025-08-04 17:11:40 +0700

    Update cde.sql

--- cde.sql
+++ cde.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 04, 2025 at 02:28 AM
+-- Generation Time: Aug 04, 2025 at 12:11 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -54,7 +54,9 @@ CREATE TABLE `notifications` (
 INSERT INTO `notifications` (`id`, `sender_id`, `receiver_id`, `entry_date`, `created_at`, `is_read`) VALUES
 (5, 4, 1, '2025-08-01', '2025-08-03 11:07:53', 1),
 (6, 4, 1, '2025-08-01', '2025-08-03 11:21:46', 1),
-(7, 4, 1, '2025-08-03', '2025-08-03 11:22:42', 1);
+(7, 4, 1, '2025-08-03', '2025-08-03 11:22:42', 1),
+(8, 1, 4, '2025-07-01', '2025-08-04 02:00:12', 1),
+(9, 4, 1, '2025-08-04', '2025-08-04 03:03:47', 1);
 
 -- --------------------------------------------------------
 
@@ -170,7 +172,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `first_name`, `last_name`, `dob`, `address`, `company`, `phone`, `invite_code`, `email`, `password_hash`, `role`, `avatar`, `subscription_id`, `company_id`, `subscription_expires_at`, `created_at`) VALUES
-(1, 'huypham', 'Phạm Mạnh', 'Huy', '1996-04-04', 'Hạ Long', 'NCC', '0888121496', NULL, 'phamhuy.cngt@gmail.com', '$2y$10$YXa/ryYZhrQyEE6rBuORVugmQSIQgmwJxBdebsPapaDuzQDaeKPhy', 'admin', 'avatar_1.png', 3, NULL, NULL, '2025-07-30 01:18:51'),
+(1, 'huypham', 'Phạm Mạnh', 'Huy', '1996-04-04', 'Hạ Long', 'NCC', '0888121496', NULL, 'phamhuy.cngt@gmail.com', '$2y$10$YXa/ryYZhrQyEE6rBuORVugmQSIQgmwJxBdebsPapaDuzQDaeKPhy', 'admin', 'avatar_1.png', 4, NULL, NULL, '2025-07-30 01:18:51'),
 (4, 'user1', 'user1a', 'user1b', '2025-07-31', 'Hạ Long', 'NCC', '0888121496', NULL, 'user1@bimtech.edu.vn', '$2y$10$wX/QF1V8VS2dHRDEeVQgFO6KFkZM.oAZaPl9ixMa.SbtH9CkGeicy', 'user', NULL, 4, NULL, '2025-12-01', '2025-07-31 04:47:19'),
 (5, 'user2', 'user2a', 'user2b', '0000-00-00', 'Hạ Long', 'NCC', '000000000', NULL, 'user2@bimtech.edu.vn', '$2y$10$joRVpVd0LQDnrM7xYP5kYuoAl6BaFqcTkFcVkK3dJKPv3LSMA5bXq', 'user', NULL, 4, NULL, NULL, '2025-08-01 13:48:30');
 
@@ -217,12 +219,121 @@ CREATE TABLE `work_diary_entries` (
 --
 
 INSERT INTO `work_diary_entries` (`user_id`, `entry_date`, `period`, `content`, `note`, `created_at`, `updated_at`) VALUES
-(4, '2025-08-01', 'morning', 'Sửa QTBT cầu Làng Lốc, Khe Pụt', NULL, '2025-08-02 07:08:04', '2025-08-02 07:08:04'),
-(4, '2025-08-01', 'afternoon', 'Sửa QTBT cầu Làng Lốc, Khe Pụt', NULL, '2025-08-02 07:08:04', '2025-08-02 07:08:04'),
-(4, '2025-08-01', 'evening', 'Break', NULL, '2025-08-02 07:08:04', '2025-08-02 07:08:04'),
-(4, '2025-08-02', 'morning', 'Cập nhật tính năng ghi nhật ký công việc trên CDE', NULL, '2025-08-02 11:40:36', '2025-08-02 11:40:36'),
-(4, '2025-08-02', 'afternoon', 'Cập nhật tính năng ghi nhật ký công việc trên CDE', NULL, '2025-08-02 11:40:36', '2025-08-02 11:40:36'),
-(4, '2025-08-02', 'evening', '17:00-19:30: Cập nhật tính năng xuất Nhật ký công việc ra Excel', NULL, '2025-08-02 11:40:36', '2025-08-02 11:40:36');
+(1, '2025-07-01', 'morning', 'Soạn văn đề nghị xác nhận áp dụng BIM dự án 330\r\nCấu hình CDE nội bộ cho các dự án BIM', NULL, '2025-08-04 02:29:06', '2025-08-04 02:29:06'),
+(1, '2025-07-01', 'afternoon', 'Chỉnh sửa lại family cống hộp', NULL, '2025-08-04 02:29:06', '2025-08-04 02:29:06'),
+(1, '2025-07-01', 'evening', 'Nghỉ', NULL, '2025-08-04 02:29:06', '2025-08-04 02:29:06'),
+(1, '2025-07-02', 'morning', 'Tạo family cống tròn và cống hộp 2 khoang', NULL, '2025-08-04 02:29:14', '2025-08-04 02:29:14'),
+(1, '2025-07-02', 'afternoon', 'Tạo family cống hộp 3 khoang và family hèm phai cống', NULL, '2025-08-04 02:29:14', '2025-08-04 02:29:14'),
+(1, '2025-07-02', 'evening', 'Nghỉ', NULL, '2025-08-04 02:29:14', '2025-08-04 02:29:14'),
+(1, '2025-07-03', 'morning', 'Tiếp tục chỉnh sửa family hèm phai cống', NULL, '2025-08-04 02:29:18', '2025-08-04 02:29:18'),
+(1, '2025-07-03', 'afternoon', 'Cấu hình CDE nội bộ phục vụ dự án BIM', NULL, '2025-08-04 02:29:18', '2025-08-04 02:29:18'),
+(1, '2025-07-03', 'evening', 'Nghỉ', NULL, '2025-08-04 02:29:18', '2025-08-04 02:29:18'),
+(1, '2025-07-04', 'morning', 'Khắc phục lỗi thiếu Packages trong Dynamo Revit', NULL, '2025-08-04 02:29:20', '2025-08-04 02:29:20'),
+(1, '2025-07-04', 'afternoon', 'Tiếp tục cấu hình CDE và phần mềm kết nối Revit với CDE', NULL, '2025-08-04 02:29:20', '2025-08-04 02:29:20'),
+(1, '2025-07-04', 'evening', 'Nghỉ', NULL, '2025-08-04 02:29:20', '2025-08-04 02:29:20'),
+(1, '2025-07-05', 'morning', 'Xây dựng thư viện thép Revit', NULL, '2025-08-04 02:29:24', '2025-08-04 02:29:24'),
+(1, '2025-07-05', 'afternoon', 'Bản vẽ cốt thép ụ neo DA 4B QL18', NULL, '2025-08-04 02:29:24', '2025-08-04 02:29:24'),
+(1, '2025-07-05', 'evening', 'Nghỉ', NULL, '2025-08-04 02:29:24', '2025-08-04 02:29:24'),
+(1, '2025-07-06', 'morning', 'Nghỉ', NULL, '2025-08-04 02:29:28', '2025-08-04 02:29:28'),
+(1, '2025-07-06', 'afternoon', 'Nghỉ', NULL, '2025-08-04 02:29:28', '2025-08-04 02:29:28'),
+(1, '2025-07-06', 'evening', 'Nghỉ', NULL, '2025-08-04 02:29:28', '2025-08-04 02:29:28'),
+(1, '2025-07-07', 'morning', 'Hoàn chỉnh thư viện thép Revit', NULL, '2025-08-04 02:29:31', '2025-08-04 02:29:31'),
+(1, '2025-07-07', 'afternoon', 'Nghiên cứu cách khắc phục lỗi không triển khai cốt thép được trên dynamo 3.5 (BIM)', NULL, '2025-08-04 02:29:31', '2025-08-04 02:29:31'),
+(1, '2025-07-07', 'evening', 'Nghỉ', NULL, '2025-08-04 02:29:31', '2025-08-04 02:29:31'),
+(1, '2025-07-08', 'morning', 'Hạ cấp và dựng lại mô hình Khe Hố từ revit 2026 về 2025 để triển khai cốt thép', NULL, '2025-08-04 02:29:34', '2025-08-04 02:29:34'),
+(1, '2025-07-08', 'afternoon', 'Viết node Dynamo hỗ trợ nhập dữ liệu excel vào Revit', NULL, '2025-08-04 02:29:34', '2025-08-04 02:29:34'),
+(1, '2025-07-08', 'evening', 'Nghỉ', NULL, '2025-08-04 02:29:34', '2025-08-04 02:29:34'),
+(1, '2025-07-09', 'morning', 'Viết node Dynamo hỗ trợ tạo cốt thép trong revit', NULL, '2025-08-04 02:29:38', '2025-08-04 02:29:38'),
+(1, '2025-07-09', 'afternoon', 'Viết node Dynamo hỗ trợ tạo cốt thép trong revit', NULL, '2025-08-04 02:29:38', '2025-08-04 02:29:38'),
+(1, '2025-07-09', 'evening', 'Nghỉ', NULL, '2025-08-04 02:29:38', '2025-08-04 02:29:38'),
+(1, '2025-07-10', 'morning', 'Viết node Dynamo hỗ trợ tạo cốt thép trong revit', NULL, '2025-08-04 02:29:41', '2025-08-04 02:29:41'),
+(1, '2025-07-10', 'afternoon', 'Viết node Dynamo hỗ trợ tạo cốt thép trong revit', NULL, '2025-08-04 02:29:41', '2025-08-04 02:29:41'),
+(1, '2025-07-10', 'evening', 'Nghỉ', NULL, '2025-08-04 02:29:41', '2025-08-04 02:29:41'),
+(1, '2025-07-11', 'morning', 'Viết node Dynamo hỗ trợ tạo cốt thép trong revit', NULL, '2025-08-04 02:29:44', '2025-08-04 02:29:44'),
+(1, '2025-07-11', 'afternoon', 'Viết node Dynamo hỗ trợ tạo cốt thép trong revit', NULL, '2025-08-04 02:29:44', '2025-08-04 02:29:44'),
+(1, '2025-07-11', 'evening', 'Nghỉ', NULL, '2025-08-04 02:29:44', '2025-08-04 02:29:44'),
+(1, '2025-07-12', 'morning', 'Tường chắn đường nối VINCOM', NULL, '2025-08-04 02:31:52', '2025-08-04 02:31:52'),
+(1, '2025-07-12', 'afternoon', 'Nghỉ Mát', NULL, '2025-08-04 02:31:52', '2025-08-04 02:31:52'),
+(1, '2025-07-12', 'evening', 'Nghỉ', NULL, '2025-08-04 02:31:52', '2025-08-04 02:31:52'),
+(1, '2025-07-13', 'morning', 'Nghỉ Mát', NULL, '2025-08-04 02:30:01', '2025-08-04 02:30:01'),
+(1, '2025-07-13', 'afternoon', 'Nghỉ', NULL, '2025-08-04 02:30:01', '2025-08-04 02:30:01'),
+(1, '2025-07-13', 'evening', 'Nghỉ', NULL, '2025-08-04 02:30:01', '2025-08-04 02:30:01'),
+(1, '2025-07-14', 'morning', 'Tường chắn đường nối VINCOM', NULL, '2025-08-04 01:40:57', '2025-08-04 01:40:57'),
+(1, '2025-07-14', 'afternoon', 'Tường chắn đường nối VINCOM', NULL, '2025-08-04 01:40:57', '2025-08-04 01:40:57'),
+(1, '2025-07-14', 'evening', '17:00-19:00: Chi tiết khác và tính khối lượng', NULL, '2025-08-04 01:40:57', '2025-08-04 01:40:57'),
+(1, '2025-07-15', 'morning', 'Tạo CDE BIM (tính năng login)', NULL, '2025-08-04 01:41:11', '2025-08-04 01:41:11'),
+(1, '2025-07-15', 'afternoon', 'Tạo CDE BIM (tính năng tạo project)', NULL, '2025-08-04 01:41:12', '2025-08-04 01:41:12'),
+(1, '2025-07-15', 'evening', '17:00-19:00: Bổ sung địa chất và chiều dài cọc tường chắn vincom', NULL, '2025-08-04 01:41:12', '2025-08-04 01:41:12'),
+(1, '2025-07-16', 'morning', 'Tạo CDE BIM (tính năng tạo project)', NULL, '2025-08-04 02:30:06', '2025-08-04 02:30:06'),
+(1, '2025-07-16', 'afternoon', 'Tạo CDE BIM (tính năng tạo mới và thay đổi thành viên)', NULL, '2025-08-04 02:30:06', '2025-08-04 02:30:06'),
+(1, '2025-07-16', 'evening', 'Nghỉ', NULL, '2025-08-04 02:30:06', '2025-08-04 02:30:06'),
+(1, '2025-07-17', 'morning', 'Tạo CDE BIM (tính năng nâng cấp chức năng cho thành viên)', NULL, '2025-08-04 02:30:09', '2025-08-04 02:30:09'),
+(1, '2025-07-17', 'afternoon', 'Tạo CDE BIM (tính năng tạo văn bản cuộc họp)', NULL, '2025-08-04 02:30:09', '2025-08-04 02:30:09'),
+(1, '2025-07-17', 'evening', 'Nghỉ', NULL, '2025-08-04 02:30:09', '2025-08-04 02:30:09'),
+(1, '2025-07-18', 'morning', 'Tạo CDE BIM (tính năng tạo văn bản cuộc họp)', NULL, '2025-08-04 01:42:06', '2025-08-04 01:42:06'),
+(1, '2025-07-18', 'afternoon', 'Tạo CDE BIM (tính năng tạo văn bản cuộc họp)', NULL, '2025-08-04 01:42:06', '2025-08-04 01:42:06'),
+(1, '2025-07-18', 'evening', '17:00-19:00: Vẽ mặt cắt ngang QL10 và 279', NULL, '2025-08-04 01:42:06', '2025-08-04 01:42:06'),
+(1, '2025-07-19', 'morning', 'Tạo CDE BIM (tính năng tạo văn bản cuộc họp)', NULL, '2025-08-04 02:30:13', '2025-08-04 02:30:13'),
+(1, '2025-07-19', 'afternoon', 'Chỉnh sửa mặt cắt ngang QL10 và 279', NULL, '2025-08-04 02:30:13', '2025-08-04 02:30:13'),
+(1, '2025-07-19', 'evening', 'Nghỉ', NULL, '2025-08-04 02:30:13', '2025-08-04 02:30:13'),
+(1, '2025-07-20', 'morning', 'Nghỉ', NULL, '2025-08-04 02:23:50', '2025-08-04 02:23:50'),
+(1, '2025-07-20', 'afternoon', 'Nghỉ', NULL, '2025-08-04 02:23:50', '2025-08-04 02:23:50'),
+(1, '2025-07-20', 'evening', 'Nghỉ', NULL, '2025-08-04 02:23:50', '2025-08-04 02:23:50'),
+(1, '2025-07-21', 'morning', 'Vẽ BTC cầu Đá Bạc và QL18B', NULL, '2025-08-04 02:30:18', '2025-08-04 02:30:18'),
+(1, '2025-07-21', 'afternoon', 'Sửa lỗi CDE BIM  (tính năng tạo văn bản cuộc họp)', NULL, '2025-08-04 02:30:18', '2025-08-04 02:30:18'),
+(1, '2025-07-21', 'evening', 'Nghỉ', NULL, '2025-08-04 02:30:18', '2025-08-04 02:30:18'),
+(1, '2025-07-22', 'morning', 'Soát khối lượng làng lốc, làng dạ và in bản vẽ Đá Bạc', NULL, '2025-08-04 02:30:21', '2025-08-04 02:30:21'),
+(1, '2025-07-22', 'afternoon', 'Họp trên ban II về thẩm định cầu Làng Lốc', NULL, '2025-08-04 02:30:21', '2025-08-04 02:30:21'),
+(1, '2025-07-22', 'evening', 'Nghỉ', NULL, '2025-08-04 02:30:21', '2025-08-04 02:30:21'),
+(1, '2025-07-23', 'morning', 'Sửa theo thẩm định Làng Lốc, Khe Pụt', NULL, '2025-08-04 02:30:24', '2025-08-04 02:30:24'),
+(1, '2025-07-23', 'afternoon', 'Đi hiện trường cầu Sông Chanh', NULL, '2025-08-04 02:30:24', '2025-08-04 02:30:24'),
+(1, '2025-07-23', 'evening', 'Nghỉ', NULL, '2025-08-04 02:30:24', '2025-08-04 02:30:24'),
+(1, '2025-07-24', 'morning', 'Bản vẽ cầu Vũ Oai', NULL, '2025-08-04 02:30:26', '2025-08-04 02:30:26'),
+(1, '2025-07-24', 'afternoon', 'Bản vẽ cầu Vũ Oai', NULL, '2025-08-04 02:30:26', '2025-08-04 02:30:26'),
+(1, '2025-07-24', 'evening', 'Nghỉ', NULL, '2025-08-04 02:30:26', '2025-08-04 02:30:26'),
+(1, '2025-07-25', 'morning', 'Sửa thẩm định Khe Pụt, slide báo cáo sông chanh, bản vẽ, khối lượng cầu Vũ Oai', NULL, '2025-08-04 02:30:33', '2025-08-04 02:30:33'),
+(1, '2025-07-25', 'afternoon', 'Họp bên ban QLBT Công trình (cầu Vũ Oai, Sông Chanh)', NULL, '2025-08-04 02:30:33', '2025-08-04 02:30:33'),
+(1, '2025-07-25', 'evening', 'Nghỉ', NULL, '2025-08-04 02:30:33', '2025-08-04 02:30:33'),
+(1, '2025-07-26', 'morning', 'Thêm PA cầu Vũ Oai', NULL, '2025-08-04 02:30:35', '2025-08-04 02:30:35'),
+(1, '2025-07-26', 'afternoon', 'Tiếp tục việc buổi sáng', NULL, '2025-08-04 02:30:35', '2025-08-04 02:30:35'),
+(1, '2025-07-26', 'evening', 'Nghỉ', NULL, '2025-08-04 02:30:35', '2025-08-04 02:30:35'),
+(1, '2025-07-27', 'morning', 'Sửa mặt cắt ngang kè Làng Lốc thẩm định', NULL, '2025-08-04 02:30:38', '2025-08-04 02:30:38'),
+(1, '2025-07-27', 'afternoon', 'Sửa các nội dung còn lại của phần tuyến Làng Lốc', NULL, '2025-08-04 02:30:38', '2025-08-04 02:30:38'),
+(1, '2025-07-27', 'evening', 'Nghỉ', NULL, '2025-08-04 02:30:38', '2025-08-04 02:30:38'),
+(1, '2025-07-28', 'morning', 'Chỉnh lại 1 số bản vẽ Làng Lốc và chỉ dẫn kỹ thuật, thuyết minh Làng Lốc, Khe Pụt', NULL, '2025-08-04 02:30:46', '2025-08-04 02:30:46'),
+(1, '2025-07-28', 'afternoon', 'Bố trí chung cầu Quảng Đức và chỉnh lại khối lượng làng lốc, khe pụt', NULL, '2025-08-04 02:30:46', '2025-08-04 02:30:46'),
+(1, '2025-07-28', 'evening', 'Nghỉ', NULL, '2025-08-04 02:30:46', '2025-08-04 02:30:46'),
+(1, '2025-07-29', 'morning', 'Tìm hiểu CDE ADS Civil', NULL, '2025-08-04 02:30:49', '2025-08-04 02:30:49'),
+(1, '2025-07-29', 'afternoon', 'Nghỉ khám bệnh', NULL, '2025-08-04 02:30:49', '2025-08-04 02:30:49'),
+(1, '2025-07-29', 'evening', 'Nghỉ', NULL, '2025-08-04 02:30:49', '2025-08-04 02:30:49'),
+(1, '2025-07-30', 'morning', 'Sửa CDE nội bộ đã viết theo chuẩn mới', NULL, '2025-08-04 02:30:51', '2025-08-04 02:30:51'),
+(1, '2025-07-30', 'afternoon', 'Sửa CDE nội bộ đã viết theo chuẩn mới', NULL, '2025-08-04 02:30:51', '2025-08-04 02:30:51'),
+(1, '2025-07-30', 'evening', 'Nghỉ', NULL, '2025-08-04 02:30:51', '2025-08-04 02:30:51'),
+(1, '2025-07-31', 'morning', 'Nghỉ lễ', NULL, '2025-08-04 02:30:53', '2025-08-04 03:44:54'),
+(1, '2025-07-31', 'afternoon', 'Nghỉ lễ : Viết báo cáo giải trình thẩm định Làng Lốc', NULL, '2025-08-04 02:30:53', '2025-08-04 03:52:34'),
+(1, '2025-07-31', 'evening', 'Nghỉ', NULL, '2025-08-04 02:30:53', '2025-08-04 02:30:53'),
+(1, '2025-08-01', 'morning', 'Quy trình bảo trì cầu Làng Lốc, Khe Pụt', NULL, '2025-08-04 10:08:37', '2025-08-04 10:08:37'),
+(1, '2025-08-01', 'afternoon', 'Quy trình bảo trì cầu Làng Lốc, Khe Pụt', NULL, '2025-08-04 10:08:37', '2025-08-04 10:08:37'),
+(1, '2025-08-01', 'evening', 'Nghỉ', NULL, '2025-08-04 10:08:37', '2025-08-04 10:08:37'),
+(1, '2025-08-02', 'morning', 'Tính năng gửi báo cáo khi viết xong nhật ký công việc', NULL, '2025-08-04 10:09:07', '2025-08-04 10:09:07'),
+(1, '2025-08-02', 'afternoon', 'Tính năng gửi báo cáo khi viết xong nhật ký công việc', NULL, '2025-08-04 10:09:07', '2025-08-04 10:09:07'),
+(1, '2025-08-02', 'evening', '17:00-19:30: Tính năng gửi báo cáo khi viết xong nhật ký công việc', NULL, '2025-08-04 10:09:07', '2025-08-04 10:09:07'),
+(1, '2025-08-03', 'morning', 'Nghỉ', NULL, '2025-08-04 09:17:41', '2025-08-04 09:17:41'),
+(1, '2025-08-03', 'afternoon', 'Nghỉ', NULL, '2025-08-04 09:17:41', '2025-08-04 09:17:41'),
+(1, '2025-08-03', 'evening', 'Nghỉ', NULL, '2025-08-04 09:17:41', '2025-08-04 09:17:41'),
+(1, '2025-08-04', 'morning', 'Cập nhật tính năng thống kê Nhật ký công việc\r\nViết giải trình thẩm định Khe Pụt', NULL, '2025-08-04 10:09:35', '2025-08-04 10:09:35'),
+(1, '2025-08-04', 'afternoon', 'Cập nhật tính năng thống kê Nhật ký công việc', NULL, '2025-08-04 10:09:35', '2025-08-04 10:09:35'),
+(1, '2025-08-04', 'evening', '17:00-19:30: Nghỉ', NULL, '2025-08-04 10:09:36', '2025-08-04 10:09:36'),
+(4, '2025-08-01', 'morning', 'Sửa QTBT cầu Làng Lốc, Khe Pụt', NULL, '2025-08-04 03:02:42', '2025-08-04 03:02:42'),
+(4, '2025-08-01', 'afternoon', 'Sửa QTBT cầu Làng Lốc, Khe Pụt', NULL, '2025-08-04 03:02:42', '2025-08-04 03:02:42'),
+(4, '2025-08-01', 'evening', 'Nghỉ', NULL, '2025-08-04 03:02:42', '2025-08-04 03:02:42'),
+(4, '2025-08-02', 'morning', 'Cập nhật tính năng ghi nhật ký công việc trên CDE', NULL, '2025-08-04 03:02:45', '2025-08-04 03:02:45'),
+(4, '2025-08-02', 'afternoon', 'Cập nhật tính năng ghi nhật ký công việc trên CDE', NULL, '2025-08-04 03:02:45', '2025-08-04 03:02:45'),
+(4, '2025-08-02', 'evening', '17:00-19:30: Cập nhật tính năng xuất Nhật ký công việc ra Excel', NULL, '2025-08-04 03:02:45', '2025-08-04 03:02:45'),
+(4, '2025-08-03', 'morning', 'Nghỉ', NULL, '2025-08-04 03:03:06', '2025-08-04 03:03:06'),
+(4, '2025-08-03', 'afternoon', 'Nghỉ', NULL, '2025-08-04 03:03:06', '2025-08-04 03:03:06'),
+(4, '2025-08-03', 'evening', 'Nghỉ', NULL, '2025-08-04 03:03:06', '2025-08-04 03:03:06'),
+(4, '2025-08-04', 'morning', 'Cập nhật tính năng thống kê nhật ký công việc trên CDE\r\nSoạn giải trình thẩm tra Khe Pụt', NULL, '2025-08-04 03:03:42', '2025-08-04 03:03:42');
 
 --
 -- Indexes for dumped tables
@@ -299,7 +410,7 @@ ALTER TABLE `companies`
 -- AUTO_INCREMENT for table `notifications`
 --
 ALTER TABLE `notifications`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `subscriptions`
