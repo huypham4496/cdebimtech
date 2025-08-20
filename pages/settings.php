d096904f26e249f956ef05e394eb366a43f93318
huypham4496 <huypham@bimtech.edu.vn>
2025-08-20 14:58:00 +0700

    Update settings.php

--- pages/settings.php
+++ pages/settings.php
@@ -119,9 +119,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Settings | CDE Bimtech</title>
-  <link rel="stylesheet" href="../assets/css/settings.css?v=<?php echo filemtime(__DIR__ . '/../assets/css/settings.css'); ?>">
-  <link rel="stylesheet" href="../assets/css/sidebar.css?v=<?php echo filemtime(__DIR__ . '/../assets/css/sidebar.css'); ?>">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+  <link rel="stylesheet" href="../assets/css/settings.css?v=<?php echo filemtime('../assets/css/settings.css'); ?>">
+  <link rel="stylesheet" href="../assets/css/sidebar.css?v=<?php echo filemtime('../assets/css/sidebar.css'); ?>">
+  <link rel="stylesheet" href="../assets/css/all.min.css?v=<?php echo filemtime('../assets/css/all.min.css'); ?>">
 </head>
 <body>
   <?php include __DIR__ . '/sidebar.php'; ?>
