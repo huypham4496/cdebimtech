288df8aa817bfc086abcb6cd8a7da95f76e32222
huypham4496 <huypham@bimtech.edu.vn>
2025-08-28 09:51:48 +0700

    Update login.php

--- pages/login.php
+++ pages/login.php
@@ -73,7 +73,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login | CDE Bimtech</title>
+  <title>Login | CDE NextInfra</title>
   <link rel="stylesheet" href="../assets/css/login.css">
 </head>
 <body>
@@ -81,7 +81,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <div class="login-left">
       <img src="../assets/images/login-bg.jpg" alt="Background">
       <div class="overlay">
-        <h1 class="text-primary">CDE Bimtech</h1>
+        <h1 class="text-primary">CDE NextInfra</h1>
         <p>Empower your workflow with real-time 3D visualization, full data ownership, and powerful BIM data analysis. Secure, immersive, and built for limitless collaboration.</p>
       </div>
     </div>
@@ -101,7 +101,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </div>
   </div>
   <div class="footer-link-wrapper">
-    &copy; 2025 a product of <a href="https://bimtech.edu.vn" class="footer-link">Bimtech</a>
+    &copy; 2025 a product of <a href="https://nextinfra.vn" class="footer-link">NextInfra</a>
   </div>
 </body>
 </html>
